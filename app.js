64fd6aad04190537013c4e2ab9dde3bc87f5becd
Tom Pocius <poci14@gmail.com>
2019-10-22 18:30:55 -0500

    attempted to add uvindex, started some css for the list city

--- app.js
+++ app.js
@@ -1,34 +1,49 @@
-const forecastURL = 'https://api.openweathermap.org/data/2.5/forecast?';
-const endBaseURL = '&units=imperial&appid=35b0242376c730106a1ead757ba5708a';
+const forecastURL = "https://api.openweathermap.org/data/2.5/forecast?";
+const endBaseURL = "&units=imperial&appid=35b0242376c730106a1ead757ba5708a";
 let forecastDisplay = $("#forecast-div");
+let previousCityDisplay = $("#previous-search-city");
+
+
+function cityUvIndex(latitude, longitude) {
+  let uvQueryURL = `api.openweathermap.org/data/2.5/uvi?appid=35b0242376c730106a1ead757ba5708a&lat=${latitude}&lon=${longitude}&cnt=5`;
+console.log('lat', latitude);
+console.log('long', longitude);
+  $.ajax({
+    url: uvQueryURL,
+    method: "GET"
+  }).then(function(uvIndex) {
+   console.log('2', uvIndex);
+
+  });
+};
+
 
 
 function searchCity(city, lat, lng) {
   let queryURL =
-    forecastURL +
-    (city ? `q=${city}` : `lat=${lat}&lon=${lng}`) +
-    endBaseURL;
+    forecastURL + (city ? `q=${city}` : `lat=${lat}&lon=${lng}`) + endBaseURL;
 
   $.ajax({
     url: queryURL,
     method: "GET"
   }).then(function(response) {
     console.log(response);
+    let latitude = response.city.coord.lat;
+    let longitude = response.city.coord.lon;
 
-    // conditions: response.list[0].weather[0].description,
-    
-    
-  // NEED to get local DATE and TIME
-
-    // Might need to use different API to link to UV index using above information for coordinates to search uv api
-
-    // let searchCityCoordinates = response.coord;
-
+    cityUvIndex(latitude, longitude);
     displayCityInfo(response);
     displayForecast(response);
     cityStorage(response.city.name);
   });
-};
+}
+
+
+
+
+
+
+
 
 function cityStorage(cityName) {
   let cityList = localStorage.getItem("key")
@@ -36,9 +51,21 @@ function cityStorage(cityName) {
     : [];
 
   cityList = cityList.concat(cityName);
+  showPreviousCity(cityList);
 
   localStorage.setItem("key", JSON.stringify(cityList));
-};
+}
+
+function showPreviousCity(cityList) {
+  for (i = 0; i < cityList.length; i++) {
+    showCityListDiv = $(
+      `<ul class="previous-city-display">
+        <li>${cityList[i]}
+        </li>
+      </ul>`);
+    previousCityDisplay.prepend(showCityListDiv);
+  }
+}
 
 function displayCityInfo(response) {
   let createDiv = $(
@@ -56,14 +83,13 @@ function displayCityInfo(response) {
     </div>`
   );
   $("#searched-city-info").html(createDiv);
-  console.log('CREAT', createDiv);
-};
+  
+}
 
 function displayForecast(response) {
-  
-  for(i = 5; i < response.list.length; i += 8) {
+  for (i = 5; i < response.list.length; i += 8) {
     forecastDiv = $(
-    `<div class="forecast-box shadow-light">
+      `<div class="forecast-box shadow-light">
         <h2>
           ${response.city.name}
         </h2>
@@ -79,14 +105,13 @@ function displayForecast(response) {
     </div>`
     );
     forecastDisplay.append(forecastDiv);
-    console.log('forecast', forecastDiv);
   }
-};
-  
+}
 
 $("#city-search").on("click", function() {
   let city = $(":text").val();
-forecastDisplay.html('');
+  forecastDisplay.html("");
+  previousCityDisplay.html("");
   searchCity(city);
 });
 
@@ -96,4 +121,4 @@ if ("geolocation" in navigator) {
   });
 } else {
   console.log("this browser does not support geolocation");
-};
+}
